964a8997f73b8a4d4d0bcf09d9b56ca644158f65
Ashish Saini <47918474+ashus3868@users.noreply.github.com>
2023-05-20 16:36:44 +0530

    Update MQTT_Robot_Control.ino

--- MQTT_Robot_Control.ino
+++ MQTT_Robot_Control.ino
@@ -1,38 +1,7 @@
-/*
-  Basic ESP8266 MQTT example
-  This sketch demonstrates the capabilities of the pubsub library in combination
-  with the ESP8266 board/library.
-  It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-  It will reconnect to the server if the connection is lost using a blocking
-  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
-  achieve the same result without blocking the main loop.
-  To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-*/
+
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-
-#define DHTPIN D5     // Digital pin connected to the DHT sensor 
-
-// Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-
-DHT_Unified dht(DHTPIN, DHTTYPE);
-
-uint32_t delayMS;
 
 // Update these with values suitable for your network.
 
@@ -45,7 +14,6 @@ PubSubClient client(espClient);
 
 unsigned long lastMsg = 0;
 String msgStr = "";
-float temp, hum;
 
 int 
 M11 = D4, //Connect Motor 1 Blue wire To Pin #13 
@@ -201,18 +169,7 @@ void loop() {
   unsigned long now = millis();
   if (now - lastMsg > 2000) {
     lastMsg = now;
-
     
-//    delay(delayMS);
-  // Get temperature event and print its value.
-    
-//    msgStr = String(temp) +","+String(hum);
-//    byte arrSize = msgStr.length() + 1;
-//    char msg[arrSize];
-//    Serial.print("PUBLISH DATA:");
-//    Serial.println(msgStr);
-//    msgStr.toCharArray(msg, arrSize);
-//    client.publishS(topic, msg);
     msgStr = "";
     delay(50);
   }
